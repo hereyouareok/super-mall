5cf5802247bb04c8c7a16376bbf92582a48a634e
we <843705011@qq.com>
2021-05-16 22:17:00 +0800

    轮播图

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -1,13 +1,40 @@
 <template>
-  <div>首页</div>
+  <div id="home">
+    <nav-bar class="home-nav"><div slot="center">购物街</div></nav-bar>
+    <home-swiper :banners="banners"/>
+  </div>
 </template>
 
 <script>
+import NavBar from "@/components/common/navbar/NavBar";
+import HomeSwiper from "@/views/home/childComps/HomeSwiper";
+import {getHomeMultiData} from "@/network/home";
+
 export default {
-  name: "Home"
+  name: "Home",
+  components: {
+    NavBar,
+    HomeSwiper
+  },
+  data() {
+    return {
+      banners: [],
+      recommends: []
+    }
+  },
+  created() {
+    getHomeMultiData().then(res => {
+      this.banners = res.data.banner.list
+      this.recommends = res.data.recommend.list
+    })
+  },
+  methods: {}
 }
 </script>
 
 <style scoped>
-
+.home-nav {
+  background-color: var(--color-tint);
+  color: #fff;
+}
 </style>
